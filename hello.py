89921cd94db3bc6f51fe27a174dbd3a44119146c
xdkoooo <421280548@qq.com>
2018-09-06 18:31:20 +0800

    hello.py add

---
+++ hello.py
@@ -0,0 +1,19 @@
+from flask import  Flask
+from flask import request
+app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return '<h1>Hello World!<h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+@app.route('/')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s</p>' % user_agent
+
+if __name__ == '__main__':
+    app.run(debug=True)
