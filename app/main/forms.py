0e696b35ec05ac9598ac0870cb5e34ed8412224f
xdkoooo <421280548@qq.com>
2018-10-07 23:18:43 +0800

    blog index html change

--- app/main/forms.py
+++ app/main/forms.py
@@ -42,3 +42,8 @@ class EditProfileAdminForm(FlaskForm):
         if field.data != self.user.username and \
             User.query.filter_by(username=field.data).first():
             raise ValidationError('Username already in use.')
+
+
+class PostForm(FlaskForm):
+    body = TextAreaField("what's on your mind?", validators=[DataRequired()])
+    submit = SubmitField('Submit')
